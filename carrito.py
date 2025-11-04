24412a3a2b4b5f9cfd0acb7e7881e06e5998d243
Sara Cuellar <saracuellar213@gmail.com>
2025-11-04 17:35:37 -0500

    ver 2

--- carrito.py
+++
@@ -1,4 +0,0 @@
-carrito = []
-
-def agregar_producto(producto, cantidad):
-    carrito.append({"producto": producto, "cantidad": cantidad})
--- principal.py
+++
@@ -1,6 +0,0 @@
-from database.conexion import crear_tablas
-from inicio_sesion import Mostrar_Inicio_Sesion
-
-if __name__ == "__main__":
-    crear_tablas()  
-    Mostrar_Inicio_Sesion()
